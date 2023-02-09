33b2ab5d52f0c059a4fdf0f53386ce7d2b153ae9
PratipalsinhRana <pratipalsinh.inexture@gmail.com>
2023-02-09 13:45:48 +0530

    add new

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,5 +2,6 @@ describe('My First Cypress Test', function() {
     it('Visits the ToolsQA Demo Page and check the menu items', function() {
     //Visit the Demo QA Website
     cy.visit("https://www.scplco.com/");
+    
 })
 })
